9c409a9014b082605504c28d9b8210dff2b632f8
isaia.caprelli <isaia.caprelli@PC7304>
2021-04-21 10:31:46 +0200

    Creazione thread 1

--- Verifica recupero debito/Verifica recupero debito/Program.cs
+++ Verifica recupero debito/Verifica recupero debito/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Verifica_recupero_debito
 {
@@ -6,7 +8,31 @@ namespace Verifica_recupero_debito
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Thread t1 = new Thread(new ThreadStart(Somma));
+            Thread t2 = new Thread(new ThreadStart(Ciclo));
+            Thread t3 = new Thread(new ThreadStart(Stampa));
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+
+            static void Somma()
+            {
+                int add1 = 10;
+                int add2 = 20;
+                int somma;
+
+                Console.WriteLine("inizio thread 1");
+                for(int i =0; i < 10; i++)
+                {
+                    Thread.Sleep(1000);
+                    somma = add1 + add2;
+                    Console.WriteLine("somma è " + somma);
+
+                }
+                Console.WriteLine("fine thread 1");
+            }
+
         }
     }
 }

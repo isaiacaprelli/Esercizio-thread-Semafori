a711580217a277ce6162de4de3c2bd549cbaf4d3
isaia.caprelli <isaia.caprelli@PC7304>
2021-04-21 10:15:40 +0200

    Master

---
+++ Verifica recupero debito/Verifica recupero debito/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Verifica_recupero_debito
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
